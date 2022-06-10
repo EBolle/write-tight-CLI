8461e0a1854d4c95459b3f292f51fb7de92bb753
EBolle <ernstbolle@gmail.com>
2022-06-10 21:58:49 +0200

    Added the html utilities module and two helper fuctions to read an url and process its content

---
+++ html_utilities.py
@@ -0,0 +1,25 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+def read_url(url: str) -> BeautifulSoup:
+    """ 
+    Processes the url to a BeatifulSoup object which holds 
+    the text of the HTML.
+    """
+    raw_html = requests.get(url)
+
+    if raw_html.status_code == 200:
+        return BeautifulSoup(raw_html.text, 'html_parser')
+    else:
+        raise ValueError(f"The HTML response is not OK: {raw_html.status_code}")
+
+def filter_tags(html_text: BeautifulSoup) -> str:
+    """
+    Filters the 'h1' and 'p' elements of the BeatifulSoup object and 
+    transforms the tags to strings.
+    """
+    html_content = html_text.find_all(['h1', 'p'])
+    html_content_as_str = [str(tag) for tag in html_content]
+
+    return ' '.join(html_content_as_str)
