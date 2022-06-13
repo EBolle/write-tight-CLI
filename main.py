d2607810707a40eccf446a37b41148c7e3e72984
EBolle <ernstbolle@gmail.com>
2022-06-13 21:55:44 +0200

    Added the launch functionality to the wt command and the creation of the tempfile

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import time
+from pathlib import Path
+
 import click 
 
 import html_utils 
@@ -7,13 +10,19 @@ import html_utils
 def wt(url):
     """
     Opens a new browser tab with the text content of the url and color-coded 
-    suggestions how to improve the text to write tight (wt).
+    suggestions on how to improve the text to write tight (wt).
     """
     html_text = html_utils.read_url(url)
     html_content = html_utils.filter_tags(html_text)
     html_content = html_utils.remove_tag_content(html_content)
 
-    click.echo(html_content)
+    Path('_temp.html').touch()
+    with open('_temp.html', mode='w') as output:
+        output.write(html_content)
+        click.launch('_temp.html')
+        time.sleep(0.5)
+
+    Path('_temp.html').unlink()
 
 if __name__ == '__main__':
     wt()
