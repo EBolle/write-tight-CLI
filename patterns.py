a319983b3d1106ca4d789c975d83e0b67bb16cc1
EBolle <ernstbolle@gmail.com>
2022-06-15 08:19:29 +0200

    Added the subjunctive mood (sm) pattern, time to refactor to adhere to DRY

--- patterns.py
+++ patterns.py
@@ -1,6 +1,6 @@
 import re
 
 ly_pattern = re.compile(r'\w+ly\b')    
+sm_pattern = re.compile(r'\b(would|should|could)\b', flags=re.IGNORECASE)
 
-
-patterns_list = [(ly_pattern, 'ly-pattern'), ]
+patterns_list = [(ly_pattern, 'ly-pattern'), (sm_pattern, 'sm-pattern')]
--- static/js/script.js
+++ static/js/script.js
@@ -6,3 +6,9 @@ const lyMatches = document.getElementsByClassName("ly-pattern");
 for (match of lyMatches) {
   match.style.backgroundColor = "orange";
 }
+
+const smMatches = document.getElementsByClassName("sm-pattern");
+
+for (match of smMatches) {
+  match.style.backgroundColor = "yellow";
+}
