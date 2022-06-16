d74eae2b420550f3863674cec14f591cb050fcc8
EBolle <ernstbolle@gmail.com>
2022-06-16 22:18:56 +0200

    Fixed the tuple conversion to the original string format in order to be able to search and replace the string

--- validation.py
+++ validation.py
@@ -10,12 +10,14 @@ from nltk.corpus import wordnet as wn
 def pv_validation(pv_matches: set) -> set:
     """
     Only keep the matches of which the second word of the match
-    is a verb.
+    is a verb. The format string is necessary to get the text back
+    to its original form instead of a tuple ('w1', 'w2').
     """
     second_word = [words[1] for words in pv_matches]
     is_verb_list = [is_verb(word) for word in second_word]
-    
-    return set(list(compress(pv_matches, is_verb_list)))
+    to_keep_list = list(compress(pv_matches, is_verb_list))
+
+    return set([f"{w1} {w2}" for w1, w2 in to_keep_list])
 
 def is_verb(word):
     return bool(wn.synsets(word, pos=wn.VERB))
