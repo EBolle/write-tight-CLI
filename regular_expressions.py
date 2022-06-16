31357120c346765edc2979ef88cd8e1dcc1b4ac0
EBolle <ernstbolle@gmail.com>
2022-06-16 21:46:56 +0200

    Added the passive voice regular expression and changed the expressions to more meaningful names

--- regular_expressions.py
+++ regular_expressions.py
@@ -1,6 +1,8 @@
 import re
 
-ly_pattern = re.compile(r'\w+ly\b')    
-sm_pattern = re.compile(r'\b(would|should|could)\b', flags=re.IGNORECASE)
+words_that_end_on_ly = re.compile(r'\w+ly\b')    
+subjunctive_mood = re.compile(r'\b(would|should|could)\b', flags=re.IGNORECASE)
+passive_voice_pattern = re.compile(r'\b(am|are|is|was|were|been|being)\b\s{1}(.+?)\b', flags=re.IGNORECASE|re.DOTALL)
 
-regular_expressions_list = [(ly_pattern, 'ly-pattern'), (sm_pattern, 'sm-pattern')]
+regular_expressions_list = [(words_that_end_on_ly, 'ly-pattern'), (subjunctive_mood, 'sm-pattern'), 
+                            (passive_voice_pattern, 'pv-pattern')]
