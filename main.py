a22b7c9ba3f804a77a904b748cf7619e99b46309
EBolle <ernstbolle@gmail.com>
2022-06-18 19:55:12 +0200

    Updated every module according to the flake8 linter

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-from re import I
 import time
 
 import click
@@ -14,8 +13,7 @@ import search_and_replace
 @click.command()
 @click.argument('url', type=click.STRING)
 def wt(url):
-    """
-    Opens a new browser tab with the text content of the url and color-coded 
+    """Opens a new browser tab with the text content of the url and color-coded
     suggestions on how to improve the text to write tight (wt).
     """
     html_content = read_and_clean_html.main(url)
@@ -25,14 +23,15 @@ def wt(url):
         if regex_name in validations:
             match[regex_name] = validations[regex_name](match[regex_name])
         html_content = search_and_replace.main(html_content, match)
-    
-    Path('_temp.html').touch() # MOVE this to a separate module
+
+    Path('_temp.html').touch()
     with open('_temp.html', mode='w') as output:
         output.write(html_content)
         click.launch('_temp.html')
         time.sleep(0.5)
 
     Path('_temp.html').unlink()
+
 
 if __name__ == '__main__':
     wt()
--- matches.py
+++ matches.py
@@ -1,9 +1,10 @@
-import re 
+import re
 
 
-def main(html_content: str, regex: re.Pattern, regex_name: str) -> dict[str, set]:
-    """
-    Matches each regular expression on the html_content and saves unique
+def main(html_content: str,
+         regex: re.Pattern,
+         regex_name: str) -> dict[str, set]:
+    """Matches each regular expression on the html_content and saves unique
     matches in a dictionary.
     """
     matches = dict()
