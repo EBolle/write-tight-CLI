8a2eb6f55a73e2ef5e73af91c5b45288895b4181
EBolle <ernstbolle@gmail.com>
2022-06-18 07:01:29 +0200

    Simplified matches and updated type hinting

--- matches.py
+++ matches.py
@@ -1,15 +1,12 @@
 import re 
 
 
-def main(html_content: str, regular_expressions_list: list) -> dict:
+def main(html_content: str, regular_expression: tuple) -> dict[str, set]:
     """
     Matches each regular expression on the html_content and saves unique
     matches in a dictionary.
     """
-    match_dict = dict()
+    matches = dict()
+    matches[regular_expression[1]] = set(re.findall(regular_expression[0], html_content))
 
-    for regular_expression in regular_expressions_list:
-        match_set = set(re.findall(regular_expression[0], html_content))
-        match_dict[regular_expression[1]] = match_set
-
-    return match_dict
+    return matches
