72e5d5b9907eaf08c78e0d9d77d366595182458a
EBolle <ernstbolle@gmail.com>
2022-06-18 07:02:59 +0200

    Simplified the main function of search and replace and updated the type hinting

--- search_and_replace.py
+++ search_and_replace.py
@@ -1,19 +1,16 @@
 
 
-def main(html_content: str, match_dict: dict) -> str:
+def main(html_content: str, match: dict[str, str]) -> str:
     """ 
-    Search and replace all matches of all patterns with span elements
-    including a class reference to the regular expressions.
+    Search and replace every match with span elements
+    including a class reference to the regular expressions name.
     """
-    for pattern, match_set in match_dict.items():
-        for match in match_set:
-            html_content = html_content.replace(match, add_span_element(match, pattern))
+    for regex_name, matches in match.items():
+        for match in matches:
+            html_content = html_content.replace(match, add_span_element(match, regex_name))
 
     return html_content
 
 
-def add_span_element(match: str, class_name: str) -> dict:
-    """
-    Wraps the match in a <span> element including the class name.
-    """
-    return f"<span class='{class_name}'>{match}</span>"
+def add_span_element(match: str, regex_name: str) -> str:
+    return f"<span class='{regex_name}'>{match}</span>"
