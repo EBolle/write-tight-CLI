eaa034c8dc4311d7fac72ffc3ee5d946160f65fd
EBolle <ernstbolle@gmail.com>
2022-06-27 22:16:36 +0200

    Started re-factoring the regular expression module by using a Pattern class which can be inherited

--- write_tight/src/regular_expressions.py
+++ write_tight/src/regular_expressions.py
@@ -1,4 +1,32 @@
 import re
+from dataclasses import dataclass
+
+
+@dataclass
+class Pattern:
+    name: str
+    pattern: re.Pattern
+    html_content: str
+
+    def main(self) -> str:
+        matches = self.match()
+        replaced_html = self.search_and_replace(matches)
+
+        return replaced_html
+
+    def match(self) -> set:
+        return set(re.findall(self.pattern, self.html_content))
+
+    def search_and_replace(self, matches: set) -> str:
+        return_html = self.html_content
+
+        for match in matches:
+            return_html.replace(match, self._add_span_element(match))
+
+        return return_html
+
+    def _add_span_element(self, match: str):
+        return f"<span class='{self.name}'>{match}</span>"
 
 
 words_that_end_on_ly = re.compile(r'\w+ly\b')
