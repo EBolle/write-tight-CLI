5cedce5b94f9da54536ee074c0a716bc81f41e40
EBolle <ernstbolle@gmail.com>
2022-07-01 21:30:35 +0200

    Added the first test on ambiguous pronouns since they were causing errors in the matching process

--- tests/test_default_patterns.py
+++ tests/test_default_patterns.py
@@ -1,7 +1,22 @@
-from re import I
 from write_tight.src.default_patterns import (
     ambiguous_pronouns,
     ambiguous_openings,
     words_ending_with_ly,
     subjunctive_mood,
 )
+
+
+def test_ambiguous_pronouns():
+    result = ambiguous_pronouns.match(
+        "https://ebolle.github.io/write-tight/styles.css"
+    )
+    expected = set()
+
+    assert result == expected
+
+
+def test_ambiguous_pronouns_2():
+    result = ambiguous_pronouns.match("git it thatthere this THOSE")
+    expected = set(["it", "this", "THOSE"])
+
+    assert result == expected
