7156a915360a32460029f0b9561f7ff1cca72215
EBolle <ernstbolle@gmail.com>
2022-07-02 20:30:15 +0200

    Added high contrast colors to the JavaScript script for better readability and understanding

--- docs/script.js
+++ docs/script.js
@@ -1,9 +1,12 @@
+// The colors need to have high contrast since there can be lots of color highlighting
+// in close proximity. Hence I have used this blog for assistance: https://sashamaps.net/docs/resources/20-colors/.
+
 const patterns = {
-  "ambiguous-pronouns": "#F692BC",
-  "ambiguous-openings": "#F4ADC6",
-  "words-ending-with-ly": "#FDFD95",
-  "subjunctive-mood": "#AAC5E2",
-  "passive-voice": "#6891C3",
+  "ambiguous-pronouns": "#469990",
+  "ambiguous-openings": "#ffd8b1",
+  "words-ending-with-ly": "#aaffc3",
+  "subjunctive-mood": "#42d4f4",
+  "passive-voice": "#e6194B",
 };
 
 for (pattern in patterns) {
