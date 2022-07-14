c01677c5ba0c010b715615f35b6a318c19394412
EBolle <ernstbolle@gmail.com>
2022-07-14 21:52:19 +0200

    Added a remove tag pattern matches test to better understand the pattern

---
+++ tests/test_read_and_clean_html.py
@@ -0,0 +1,29 @@
+import re
+
+import pytest
+
+from write_tight.src.read_and_clean_html import GetHtmlContent
+
+
+@pytest.fixture
+def sample_text() -> str:
+    text = """<h1 thistextshouldberemoved>and this text should not,
+    basically all characters between <p class=certain-elements> but
+    what about fictive tags like <lolly what about spaces>?
+    """
+    return text
+
+
+@pytest.fixture
+def ghc_instance() -> GetHtmlContent:
+    ghc_instance = GetHtmlContent(url="")
+    return ghc_instance
+
+
+def test_remove_tag_pattern_matches(
+    sample_text: str, ghc_instance: GetHtmlContent
+):
+    test_input = re.findall(ghc_instance.tag_pattern, sample_text)
+    expected = [("<h1", ">"), ("<p", ">"), ("<lolly", ">")]
+
+    assert test_input == expected
