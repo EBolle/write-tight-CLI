b2ce1bb3303f75fa76c772b9b36aaed2645af568
EBolle <ernstbolle@gmail.com>
2022-07-15 20:43:29 +0200

    launch_html now writes the rendered templated with ninja2. The Markup funtion is necessary for Jinja2 to not autoescape the html tags for better security. Hence I left the autoescape on but since I am using trusted data this is ok

--- write_tight/src/launch_html.py
+++ write_tight/src/launch_html.py
@@ -2,13 +2,29 @@ import time
 from pathlib import Path
 
 import click
+from jinja2 import Environment, PackageLoader, select_autoescape
+from markupsafe import Markup
+
+import write_tight.src.config as config
+
+
+env = Environment(
+    loader=PackageLoader("write_tight"), autoescape=select_autoescape()
+)
+post_template = env.get_template("post.html")
 
 
 def launch_html(html_content: str) -> None:
     Path("_temp.html").touch()
 
     with open("_temp.html", mode="w") as output:
-        output.write(html_content)
+        output.write(
+            post_template.render(
+                css_link=config.CSS_URL,
+                content=Markup(html_content),
+                js_link=config.JS_URL,
+            )
+        )
         click.launch("_temp.html")
         time.sleep(0.5)
 
